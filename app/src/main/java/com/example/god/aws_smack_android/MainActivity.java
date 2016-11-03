eafdf0bbdc791fbdea17ad8a575ce3129ca1149c
diffth@nate.com <diffth@nate.com>
2016-11-04 03:51:35 +0900

    Openfire RestAPI 추가

--- app/src/main/java/com/example/god/aws_smack_android/MainActivity.java
+++ app/src/main/java/com/example/god/aws_smack_android/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.god.aws_smack_android;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.igniterealtime.restclient.RestApiClient;
+import org.igniterealtime.restclient.entity.AuthenticationToken;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //http://52.78.215.218/:9090/plugins/restapi/v1/users
+        //http://52.78.215.218:9090/plugins/restapi/v1/users
+        // Basic HTTP Authentication
+        AuthenticationToken authenticationToken = new AuthenticationToken("admin", "qwer!@#$");
+
+        RestApiClient restApiClient = new RestApiClient("http://52.78.215.218", 9090, authenticationToken);
+
+        //Log.v("=============================== ", restApiClient.getUsers().toString());
     }
 }
+
+
