315a17f6ba74cbb1c7a743463967bca7e34382d1
diffth@nate.com <diffth@nate.com>
2016-11-03 13:33:46 +0900

    first commit

---
+++ app/src/main/java/com/example/god/aws_smack_android/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.god.aws_smack_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //http://52.78.215.218/:9090/plugins/restapi/v1/users
+    }
+}
